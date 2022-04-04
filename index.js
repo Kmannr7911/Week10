9b002b7ba2d39cb4d0e070526c474632c26df012
ConnorK26 <cklingmnn5197@gmail.com>
2022-04-04 12:49:06 -0400

    Week10

---
+++ index.js
@@ -0,0 +1,56 @@
+// let button = document.getElementById('btn');
+// let content = document.getElementById('content');
+
+// button.addEventListener('click', () => {
+//         if(content.innerHTML == 'Goodbye.'){
+//             content.innerHTML = 'Hello.';
+//         } else {
+//             content.innerHTML = 'Goodbye.';
+//         }
+//     });
+
+// document.getElementById('remove').addEventListener('click', () => {
+//     let idToRemove = document.getElementId('remove-id').value;
+//     let elementToRemove = document.getElementById('idToRemove');
+//     content.parentNode.removeChild(content);
+//     document.getElementId('remove-id').value = '';
+// });
+// document.getElementById('add').addEventListener('click', () => {
+//     var parent = document.getElementById('paragraphs');
+//     var newElement = document.createElement('p');
+//     newElement.innerHTML = document.getElementById('new-text').value;
+//     newElement.setAttribute('id', id++);
+//     parent.appendChild(newElement);
+//     document.getElementById('new-text').value = '';
+// });
+
+
+
+// let button = document.getElementById('btn');
+// let content = document.getElementById('content');
+
+// button.addEventListener('click', () => {
+//     if(content.innerHTML == 'Goodbye.'){
+//         content.innerHTML = 'Hello.';
+//     } else {
+//         content.innerHTML = 'Goodbye.';
+//     }
+// });
+
+// document.getElementById('remove').addEventListener('click', () => {
+//     let idToRemove = document.getElementId('remove-id').value;
+//     let elementToRemove = document.getElementById('idToRemove');
+//     content.parentNode.removeChild(content);
+//     document.getElementId('remove-id').value = '';
+// });
+
+// let id = 0;
+
+// document.getElementById('add').addEventListener('click', () => {
+//     var parent = document.getElementById('paragraphs');
+//     var newElement = document.createElement('p');
+//     newElement.innerHTML = document.getElementById('new-text').value;
+//     newElement.setAttribute('id', id++);
+//     parent.appendChild(newElement);
+//     document.getElementById('new-text').value = '';
+// });
